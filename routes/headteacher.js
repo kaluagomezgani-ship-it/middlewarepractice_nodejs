232e6fe3b0e99d98855be09b8c254b93eec110ed
kaluagomezgani-ship-it <kaluagomezgani@gmail.com>
2026-07-08 19:02:02 +0200

    added a check middleware

--- routes/headteacher.js
+++ routes/headteacher.js
@@ -1,8 +1,18 @@
 //imports
 const router=require("express").Router();
 
+//middleware
+function checkifadmin(req,res,next){
+    if(req.query.admin==="false"){
+        return res.send("you are not an admin")
+    }
+    else{
+        console.log("welcome admin")
+        next();
+    }
+}
 //routes
-router.get("/",(req,res)=>{
+router.get("/admin",checkifadmin,(req,res)=>{
     res.send("this is a headteachers account")
 })
 
